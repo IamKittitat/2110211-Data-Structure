f796a4ed84c66aac6f43a676fbc2c6ae2e4e646a
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-12 09:50:50 +0700

    d63_q2a_pair_gte

---
+++ grader/d63_q2a_pair_gte/student.h
@@ -0,0 +1,11 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+template <typename T1,typename T2>
+bool CP::pair<T1,T2>::operator>=(const pair<T1,T2>& other) const {
+  //write your code here
+    if(this->first != other.first) return this->first >= other.first;
+    return this->second >= other.second;
+}
+
+#endif
