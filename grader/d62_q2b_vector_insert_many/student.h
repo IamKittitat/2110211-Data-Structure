34ef435f0716e769c3803021017692cc2e69922b
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-16 12:00:12 +0700

    d62_q2b_vector_insert_many

---
+++ grader/d62_q2b_vector_insert_many/student.h
@@ -0,0 +1,28 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include<bits/stdc++.h>
+template <typename T>
+void CP::vector<T>::insert_many(CP::vector<std::pair<int,T>> data) {
+  //write your code here
+  sort(data.begin(),data.end());
+  T *newMData = new T[size()+data.size()];
+  int idx = 0,idx2=0;
+  for(int i = 0 ;i<=mSize;i++){
+    if(data[idx].first == i){
+        newMData[idx2] = data[idx].second;
+        idx++;
+        idx2++;
+    }
+    if(i<mSize){
+       newMData[idx2] = mData[i];
+        idx2++;
+    }
+  }
+  delete [] mData;
+  mData = newMData;
+  mSize = size()+data.size();
+  mCap = mSize;
+}
+
+#endif
