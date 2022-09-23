4fb272d89774bb132e08d2687ce518ec9676c63c
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-23 14:55:57 +0700

    d63_q3b_queue_b2f

--- grader/d63_q3b_queue_b2f/student.h
+++ grader/d63_q3b_queue_b2f/student.h
@@ -8,19 +8,11 @@
 template <typename T>
 void CP::queue<T>::back_to_front() {
   //write your code here
-    T tmp = front();
-    std::cout << "CHECK " << tmp << std::endl;
-    std::cout << "CHECK2 " << mData[mFront] << std::endl;
-    mData[mFront] = back();
-    std::cout << "CHECK3 " << mData[mFront] << std::endl;
-    std::cout << "CHECK3 " << mData[(mFront + mSize - 1) % mCap] << std::endl;
-    mData[(mFront + mSize - 1) % mCap] = tmp;
-    std::cout << "CHECK4 " << mData[(mFront + mSize - 1) % mCap] << std::endl;
-    /*if(!empty()){
+    if(!empty()){
         T tmp = back();
         mFront = (mFront + mCap - 1)%mCap;
         mData[mFront] = tmp;
-    }*/
+    }
 }
 
 #endif
