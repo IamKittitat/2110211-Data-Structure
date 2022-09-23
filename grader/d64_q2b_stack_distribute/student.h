be63947696a595b6e6c7394bc822738397d0ea59
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-23 10:48:02 +0700

    d64_q2b_stack_distribute

---
+++ grader/d64_q2b_stack_distribute/student.h
@@ -0,0 +1,27 @@
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+template <typename T>
+std::vector<std::vector<T>> CP::stack<T>::distribute(size_t k) const {
+  //write your code here
+    std::vector<std::vector<T>> ans;
+    int idx = size()-1;
+    int si;
+    while(idx >= 0){
+        if((idx+1)%k != 0){
+            si = (idx+1)/k + 1;
+        } else{
+            si = (idx+1)/k;
+        }
+        std::cout << idx << " " << si << std::endl;
+        k--;
+        std::vector<T> subans;
+        for(int i = 0 ; i < si ; i++){
+            subans.push_back(mData[idx]);
+            idx--;
+        }
+        ans.push_back(subans);
+    }
+    return ans;
+}
+#endif
