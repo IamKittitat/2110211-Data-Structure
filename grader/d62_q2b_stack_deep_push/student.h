08f040dfa037257d94da497da1a21c681787408a
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-23 09:57:27 +0700

    d62_q2b_stack_deep_push

---
+++ grader/d62_q2b_stack_deep_push/student.h
@@ -0,0 +1,22 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+
+template <typename T>
+void CP::stack<T>::deep_push(size_t pos,const T& value) {
+  //write your code here
+    CP::stack<T> s;
+    for(size_t i = 0 ; i < pos ;i ++){
+        if(!empty()){
+            s.push(top());
+            pop();
+        }
+    }
+    push(value);
+    while(!s.empty()){
+        push(s.top());
+        s.pop();
+    }
+}
+
+#endif
