6dee5c396c185545a895f2ce5594ac59436f1a27
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-23 08:09:23 +0700

    d64_q2c_vector_less

--- grader/d64_q2c_vector_less/student.h
+++ grader/d64_q2c_vector_less/student.h
@@ -5,8 +5,7 @@ template <typename T>
 bool CP::vector<T>::operator<(const CP::vector<T> &other) const {
   //write your code here
   // if you use std::vector, your score will be half (grader will report score BEFORE halving)
-    if(empty() && other.size()>0) return true;
-    for(int i = 0 ; i < size();i++){
+    for(size_t i = 0 ; i < size();i++){
         if(i>=other.size()) return false;
         if(mData[i] < other[i]) return true;
         if(mData[i] > other[i]) return false;
