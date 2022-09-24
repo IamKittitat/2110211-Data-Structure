762949e89f87288c3a24fa759c24e419a5d7dee2
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-24 10:09:30 +0700

    d60_q2_split_queue

---
+++ grader/d60_q2_split_queue/student.h
@@ -0,0 +1,25 @@
+#ifndef _CP_STUDENT_QUEUE_INCLUDED_
+#define _CP_STUDENT_QUEUE_INCLUDED_
+
+#include <stdexcept>
+#include <iostream>
+//#pragma once
+#include <vector>
+#include <queue>
+#include "queue.h"
+
+template <typename T>
+std::vector< CP::queue<T> > CP::queue<T>::split_queue(int k) {
+    std::vector<CP::queue<T> > qs(k);
+    // code here
+    int idx = 0 ;
+    while(!empty()){
+        qs[idx%k].push(front());
+        pop();
+        idx++;
+    }
+    return qs;
+}
+
+#endif
+
