ca921ac2031b7a8a671296982125c3620d0b03b2
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-09-24 09:40:48 +0700

    d62_q3b_queue_m2f

---
+++ grader/d62_q3b_queue_m2f/student.h
@@ -0,0 +1,24 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+
+#include <algorithm>
+#include <iostream>
+
+template <typename T>
+void CP::queue<T>::move_to_front(size_t pos) {
+    //your code here
+    T *newMData = new T[mCap]();
+    newMData[0] = mData[(mFront+pos)%mCap];
+    int idx = 1;
+    for(int i = 0;i<mSize;i++){
+        std::cout << "CHECK " << i << " " << mData[(mFront+i)%mCap] << std::endl;
+        if(i != pos){
+            newMData[idx++] = mData[(mFront+i)%mCap];
+        }
+    }
+    delete [] mData;
+    mData = newMData;
+    mFront = 0;
+}
+
+#endif
