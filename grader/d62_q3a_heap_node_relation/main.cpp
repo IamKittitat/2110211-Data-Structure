baf6f0f53e19104c0a4c0f34f51b219b80653fe2
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-10-23 12:32:51 +0700

    Updated d62_q3a_heap_node_relation

--- grader/d62_q3a_heap_node_relation/main.cpp
+++ grader/d62_q3a_heap_node_relation/main.cpp
@@ -3,47 +3,34 @@
 using namespace std;
 
 bool findRelation(int child,int ancestor){
+    while(child > ancestor){
+        child = (child-1)/2;
+        if(child == ancestor) return true;
+    }
+    return false;
 }
 
 int main()
 {
-    int n,m;
-    cin >> n >> m;
-    for(int i = 0;i<m;i++){
-        queue<int> q;
+   int n,m;
+   cin >> n >> m;
+   for(int i=0;i<m;i++){
         int a,b;
-        int ans = false;
+        bool ans = false;
         cin >> a >> b;
-        if(a == b){
-            cout << "a and b are the same node\n";
-            ans = true;
+        if(a==b){
+           cout << "a and b are the same node\n";
+           ans = true;
         }
-        else if(a >b){
-            q.push(b);
-            while(!q.empty()){
-                int lc = q.front()*2+1, rc = q.front()*2+2; q.pop();
-                if(lc == a || rc == a ){
-                    cout << "b is an ancestor of a\n";
-                    ans = true;
-                    break;
-                }
-                if(lc < a) q.push(lc);
-                if(rc < a) q.push(rc);
-            }
+        else if(a>b){
+            ans =  findRelation(a,b);
+            if(ans) cout << "b is an ancestor of a\n";
         }
-        else{
-            q.push(a);
-            while(!q.empty()){
-                int lc = q.front()*2+1, rc = q.front()*2+2; q.pop();
-                if(lc == b || rc == b ){
-                    cout <<  "a is an ancestor of b\n";
-                    ans = true;
-                    break;
-                }
-                if(lc < b) q.push(lc);
-                if(rc < b) q.push(rc);
-            }
+        else if(a<b){
+            ans =  findRelation(b,a);
+            if(ans) cout << "a is an ancestor of b\n";
         }
+
         if(!ans) cout << "a and b are not related\n";
-    }
+   }
 }
