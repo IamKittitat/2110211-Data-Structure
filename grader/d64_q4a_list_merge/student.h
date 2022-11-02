8c1623eb343dd2cf90e2593a4ec17a2ffaaa1268
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-11-02 10:17:10 +0700

    d64_q4a_list_merge

---
+++ grader/d64_q4a_list_merge/student.h
@@ -0,0 +1,29 @@
+#ifndef __STUDENT_H_
+#define __STUDENT_H_
+#include <algorithm>
+
+template <typename T>
+void CP::list<T>::merge(CP::list<CP::list<T>> &ls) {
+  //write your code here
+    for(CP::list<T> &l : ls){
+        mSize+=l.size();
+        mHeader->prev->next = l.mHeader->next; //pink
+        l.mHeader->next->prev = mHeader->prev; //red
+        mHeader->prev = l.mHeader->prev; //yellow
+        l.mHeader->prev->next = mHeader; //brown
+
+        l.mHeader->next = l.mHeader;
+        l.mHeader->prev = l.mHeader;
+        l.mSize = 0;
+    }
+}
+
+#endif
+/*
+3 4
+1 2 3
+3 10 20 30
+1 100
+0
+3 990 991 992
+*/
