72ab75696cea5a5391934c925bf0d7140de14da8
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-11-11 13:25:24 +0700

    d58_q3_list_replace HOF

--- grader/d58_q3_list_replace/replace.h
+++ grader/d58_q3_list_replace/replace.h
@@ -3,26 +3,8 @@ void replace(const T& x, list<T>& y) {
     auto it = begin();
     while(it != end()){
         if(*it == x){
-            list<T> y_tmp = y;
-            node *tmp = it.ptr,*first = y_tmp.mHeader->next,*last = y_tmp.mHeader->prev;
-            it++;
-
-            if(!y.empty()){
-                tmp->prev->next = first;
-                tmp->next->prev = last;
-                first->prev = tmp->prev;
-                last->next = tmp->next;
-                delete tmp;
-
-                y_tmp.mHeader->next = y_tmp.mHeader;
-                y_tmp.mHeader->prev = y_tmp.mHeader;
-                y_tmp.mSize = 0;
-            } else{
-                tmp->prev->next = tmp->next;
-                tmp->next->prev = tmp->prev;
-                delete tmp;
-            }
-            mSize = mSize + y.size() - 1;
+             for(auto l:y) insert(it,l);
+             it = erase(it);
         }else{
             it++;
         }
