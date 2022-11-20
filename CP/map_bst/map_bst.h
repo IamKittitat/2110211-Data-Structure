fdfdeb4a15d442bcb2daffa5f00a0d916eb81063
Kittitat Tuntisak <kittitattuntisak@hotmail.com>
2022-11-20 12:40:19 +0700

    update CP_map_bst

--- CP/map_bst/map_bst.h
+++ CP/map_bst/map_bst.h
@@ -8,27 +8,26 @@ namespace CP{
             typedef std::pair<KeyT,MappedT> ValueT;
             class node{
                 friend class map_bst;
-                protected:
+                public:
                     ValueT data;
                     node *left,*right,*parent;
-                public:
-                    node(): data(ValueT()),left(NULL),right(NULL),parent(NULL){}
-                    node(const ValueT &data,node* left,node* right,node* parent): data(data),left(left),right(right),parent(parent){}
+
+                    node():data(ValueT()),left(NULL),right(NULL),parent(NULL){}
+                    node(const ValueT& data,node *left,node *right,node *parent):data(data),left(left),right(right),parent(parent){}
             };
-            //-------------------------------------------------------------
             class tree_iterator{
                 protected:
-                    node * ptr;
+                    node* ptr;
                 public:
-                    tree_iterator(): ptr(NULL){}
-                    tree_iterator(node *a): ptr(a){}
+                    tree_iterator():ptr(NULL){}
+                    tree_iterator(node* a):ptr(a){}
 
                     tree_iterator& operator++(){
                         if(ptr->right == NULL){
-                            node* parent = ptr->parent;
+                            node *parent = ptr->parent;
                             while(parent != NULL && parent->right == ptr){
                                 ptr = parent;
-                                parent = ptr -> parent;
+                                parent = ptr->parent;
                             }
                             ptr = parent;
                         } else{
@@ -37,27 +36,26 @@ namespace CP{
                         }
                         return *this;
                     }
-
                     tree_iterator& operator--(){
                         if(ptr->left == NULL){
-                            node* parent = ptr->parent;
-                            while(parent != NULL && parent->left != ptr){
+                            node *parent = ptr->parent;
+                            while(parent != NULL && parent->left == ptr){
                                 ptr = parent;
                                 parent = ptr->parent;
                             }
+                            ptr = parent;
                         } else{
                             ptr = ptr->left;
                             while(ptr->right != NULL) ptr = ptr->right;
                         }
+                        return *this;
                     }
-
-                    tree_iterator operator++(int) {
+                    tree_iterator& operator++(int){
                         tree_iterator tmp(*this);
                         operator++();
                         return tmp;
                     }
-
-                    tree_iterator operator--(int) {
+                    tree_iterator& operator--(int){
                         tree_iterator tmp(*this);
                         operator--();
                         return tmp;
@@ -65,82 +63,77 @@ namespace CP{
 
                     ValueT& operator*(){return ptr->data;}
                     ValueT* operator->(){return &(ptr->data);}
-                    bool    operator==(const tree_iterator& other) { return other.ptr == ptr; }
-                    bool    operator!=(const tree_iterator& other) { return other.ptr != ptr; }
-
-
+                    bool operator==(const tree_iterator& other){return ptr == other.ptr;}
+                    bool operator!=(const tree_iterator& other){return ptr != other.ptr;}
             };
-            //-------------------------------------------------------------
-            node *mRoot;
-            CompareT mLess;
-            size_t mSize;
-
         public:
             typedef tree_iterator iterator;
-
         protected:
-            node*& child_link(node *parent,const KeyT &k){
-                if(parent == NULL) return mRoot;
-                return mLess(parent->data.first,k) ? parent->right : parent->left;
-            }
-            int compare(const KeyT& k1,const KeyT& k2){
+            node *mRoot;
+            size_t mSize;
+            CompareT mLess;
+
+            int compare(const KeyT& k1, const KeyT& k2){
                 if(mLess(k1,k2)) return -1;
                 if(mLess(k2,k1)) return 1;
                 return 0;
             }
-            node* find_node(const KeyT& k,node* r,node* &parent){
-                node *ptr = r;
-                while(ptr != NULL){
-                    int cmp = compare(k,ptr->data.first);
-                    if(cmp == 0) return ptr;
-                    parent = ptr;
-                    ptr = cmp > 0 ? ptr->right : ptr->left;
+
+            node* find_node(const KeyT& k,node* r, node* &parent){
+                while(r != NULL){
+                    int cmp = compare(k,r->data.first);
+                    if(cmp == 0) return r;
+                    parent = r;
+                    r = cmp < 0 ? r->left : r->right;
+
                 }
                 return NULL;
             }
-            node* find_min_node(node* r){
-                node* ptr = r;
-                while(ptr->left != NULL){
-                    ptr = ptr->left;
-                }
-                return ptr;
+
+            node*& child_link(node* parent,KeyT key){
+                if(parent == NULL) return mRoot;
+                return mLess(key,parent->data.first) ? parent->left: parent->right ;
             }
-            node* find_max_node(node* r){
-                node* ptr = r;
-                while(ptr->right != NULL){
-                    ptr = ptr->right;
-                }
-                return ptr;
+
+            node* find_min_node(node *n){
+                if(n->left == NULL) return n;
+                return find_min_node(n->left);
             }
-            void delete_all_nodes(node *r){
-                if(r == NULL) return;
-                delete_all_nodes(r->left);
-                delete_all_nodes(r->right);
-                delete r;
-            }
-            node* copy(node* src, node* parent){
-                if (src == NULL) return NULL;
-                node* tmp   = new node();
-                tmp->data   = src->data;
-                tmp->left   = copy(src->left,  tmp);
-                tmp->right  = copy(src->right, tmp);
+
+
+            node* copy(node* n,node* parent){
+                if(n == NULL) return NULL;
+                node *tmp = new node();
+                tmp->data = n->data;
+                tmp->left = copy(n->left,tmp);
+                tmp->right = copy(n->right,tmp);
                 tmp->parent = parent;
                 return tmp;
+            }
+
+            void delete_all_nodes(node * n){
+                if (n == NULL) return;
+                delete_all_nodes(n->left);
+                delete_all_nodes(n->right);
+                delete n;
             }
 
 
         public:
             //-------------- constructor & copy operator ----------
+
             // default constructor
             map_bst(const CompareT& c = CompareT()): mRoot(NULL),mSize(0),mLess(c){}
 
             // copy constructor
-            map_bst(const map_bst<KeyT,MappedT,CompareT> & other): mLess(other.mLess),mSize(other.mSize){
-                mRoot = copy(other.mRoot,NULL);
+            map_bst(map_bst<KeyT,MappedT,CompareT>& a){
+                mRoot = copy(a.mRoot,NULL);
+                mSize = a.mSize;
+                mLess = a.mLess;
             }
 
             // copy assignment operator using copy-and-swap idiom
-            map_bst<KeyT,MappedT,CompareT>& operator=(map_bst<KeyT,MappedT,CompareT> other){
+            map_bst<KeyT,MappedT,CompareT>& operator=(map_bst<KeyT,MappedT,CompareT> other)  {
                 using std::swap;
                 swap(this->mRoot, other.mRoot);
                 swap(this->mLess, other.mLess);
@@ -148,7 +141,7 @@ namespace CP{
                 return *this;
             }
 
-            ~map_bst() {
+            ~map_bst(){
                 clear();
             }
 
@@ -162,86 +155,91 @@ namespace CP{
             }
 
             //----------------- iterator ---------------
-            iterator begin() {
-                return iterator(mRoot==NULL ? NULL : find_min_node(mRoot));
+            iterator begin(){
+                return iterator(mRoot == NULL ? NULL : find_min_node(mRoot));
             }
-
-            iterator end() {
+            iterator end(){
                 return iterator(NULL);
             }
 
             //----------------- modifier -------------
             void clear(){
                 delete_all_nodes(mRoot);
-                mSize = 0;
                 mRoot = NULL;
-            }
-            iterator find(const KeyT &key){
-                node* parent = NULL;
-                node *ptr = find_node(key,mRoot,parent);
-                return ptr == NULL ? end() : iterator(ptr);
+                mSize = 0;
             }
 
-
-            std::pair<iterator,bool> insert(const ValueT& val){
-                node* parent = NULL;
-                node* ptr = find_node(val.first,mRoot,parent);
-                bool not_found = (ptr == NULL);
-                if(not_found){
-                    ptr = new node(val,NULL,NULL,parent);
-                    child_link(parent,val.first) = ptr;
+            MappedT& operator[](const KeyT& key){
+                node *parent = NULL;
+                node *n = find_node(key,mRoot,parent);
+                if(n == NULL){
+                    n = new node({key,MappedT()},NULL,NULL,parent);
+                    child_link(parent,key) = n;
                     mSize++;
                 }
-                return {iterator(ptr),not_found};
+                return n->data.second;
             }
+
+            std::pair<iterator,bool> insert(const ValueT& val){
+                node *parent = NULL;
+                node *n = find_node(val.first,mRoot,parent);
+                bool not_found = (n == NULL);
+                if(not_found){
+                    n = new node(val,NULL,NULL,parent);
+                    child_link(parent,val.first) = n;
+                    mSize++;
+                }
+                return {iterator(n),not_found};
+            }
+
+            iterator find(const KeyT &key){
+                node *parent = NULL;
+                node *n = find_node(key,mRoot,parent);
+                return n == NULL ? end() : iterator(n);
+            }
+
             size_t erase(const KeyT &key){
                 if(mRoot == NULL) return 0;
 
-                node* parent = NULL;
-                node* ptr = find_node(key,mRoot,parent);
-                if(ptr == NULL) return 0;
-                if(ptr->left != NULL && ptr->right != NULL){ // Two children
-                    node* min = find_min_node(ptr->right);
-                    node* &link = child_link(min->parent,min->data.first);
+                node *parent = NULL;
+                node *n = find_node(key,mRoot,parent);
+                if(n == NULL) return 0;
+                if(n->left != NULL && n->right != NULL){
+                    node *min = find_min_node(ptr->right);
+                    node * &link = child_link(min->parent, min->data.first);
                     link = (min->left == NULL) ? min->right : min->left;
                     if (link != NULL) link->parent = min->parent;
-                    std::swap(ptr->data.first,min->data.first);
-                    std::swap(ptr->data.second,min->data.second);
-                    ptr = min;
+                    std::swap(ptr->data.first, min->data.first);
+                    std::swap(ptr->data.second, min->data.second);
+                    ptr = min; // we are going to delete this node instead
                 } else{
-                    node* &link = child_link(ptr->parent,key);
-                    link = ptr->left == NULL ? ptr->right : ptr->left;
-                    if (link != NULL) link->parent = ptr->parent;
+                    node * &link = child_link(n->parent, key);
+                    link = (n->left == NULL) ? n->right : n->left;
+                    if (link != NULL) link->parent = n->parent;
                 }
-                delete ptr;
+                delete n;
                 mSize--;
                 return 1;
+                return 1;
             }
-            MappedT& operator[](const KeyT& key){
-                node *parent = NULL;
-                node *ptr = find_node(key,mRoot,parent);
-                if(ptr == NULL){
-                    ptr = new node({key,MappedT()},NULL,NULL,parent);
-                    child_link(parent,key) = ptr;
-                    mSize++;
-                }
-                return ptr->data.second;
-            }
-            void print() {
-                std::cout << " ======== size = " << mSize << " ========= " << std::endl;
-                print_node(mRoot,0);
-            }
-            void print_node(node *n,size_t depth){
+
+
+            void print_node(node *n,size_t depth) {
                 if (n != NULL) {
                     if (n->right != NULL && n->right->parent != n)
-                        std::cout << "parent of " << n->right->data.first << std::endl;
+                      std::cout << "parent of " << n->right->data.first << std::endl;
                     if (n->left != NULL && n->left->parent != n)
-                        std::cout << "parent of " << n->left->data.first << std::endl;
+                      std::cout << "parent of " << n->left->data.first << std::endl;
                     print_node(n->right,depth+1);
                     for (size_t i = 0;i < depth;i++) std::cout << "--";
                     std::cout << " " << n->data.first << ":" << n->data.second << std::endl;
                     print_node(n->left,depth+1);
                 }
+            }
+
+            void print() {
+                std::cout << " ======== size = " << mSize << " ========= " << std::endl;
+                print_node(mRoot,0);
             }
             bool checkParent() {
                 return checkParent(mRoot);
@@ -263,9 +261,6 @@ namespace CP{
                 if (!checkInorder(r->left)) return false;
                 return checkInorder(r->right);
             }
-
-
-
 
     };
 }
